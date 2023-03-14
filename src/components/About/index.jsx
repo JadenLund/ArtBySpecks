867b1d3499422f05804952f5ea04fcd073b675a0
JadenLund <lundjaden@gmail.com>
2023-03-14 18:16:55 -0500

    Adding basic about page parts

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,9 +1,11 @@
 import './index.css'
+import ProfilePhoto from '../../assets/ProfilePhoto.jpg'
 
 export default function About() {
     return (
         <div>
-
+            <img className='ProfilePhoto' src={ProfilePhoto} alt='ProfilePhoto' />
+            <p className="About-me-text">Insert Generic paragraph</p>
         </div>
     )
 }
