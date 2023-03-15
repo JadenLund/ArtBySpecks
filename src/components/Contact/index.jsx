ed13ccc2ca8cbc02d6c245204e873c3030ccbdbe
JadenLund <lundjaden@gmail.com>
2023-03-15 14:49:23 -0500

    changing username links

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,5 +1,5 @@
 import './index.css'
-import { faArtstation, faDeviantart, faDiscord, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faArtstation, faDeviantart, faDiscord, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Contact() {
@@ -9,16 +9,17 @@ export default function Contact() {
                 <a target='_blank' className='Instagram' rel='noreferrer' href='https://www.instagram.com/critterbug.art/'>
                     <FontAwesomeIcon icon={faInstagram} color='#99738e' />
                 </a>
-                <a target='_blank' className='DeviantArt' rel='noreferrer' href='https://www.deviantart.com/critterbugart'>
+                {/* change the one above */}
+                <a target='_blank' className='Twitter' rel='noreferrer' href='https://twitter.com/ArtbySpecks'>
+                    <FontAwesomeIcon icon={faTwitter} color='#99738e' />
+                </a>
+                <a target='_blank' className='DeviantArt' rel='noreferrer' href='https://www.deviantart.com/artbyspecks'>
                     <FontAwesomeIcon icon={faDeviantart} color='#99738e' />
                 </a>
                 <a target='_blank' className='Youtube' rel='noreferrer' href='https://www.youtube.com/channel/UCv0NJ7z_5-Sm7Y1QpKzy2eg'>
                     <FontAwesomeIcon icon={faYoutube} color='#99738e' />
                 </a>
-                <a target='_blank' className='Discord' rel='noreferrer' href='https://discord.gg/JNgcm7vjNz'>
-                    <FontAwesomeIcon icon={faDiscord} color='#99738e' />
-                </a>
-                <a target='_blank' className='Artstation' rel='noreferrer' href='https://www.artstation.com/critterbugart'>
+                <a target='_blank' className='Artstation' rel='noreferrer' href='https://www.artstation.com/artbyspecks'>
                     <FontAwesomeIcon icon={faArtstation} color='#99738e' />
                 </a>
             </div>
--- src/components/Store/index.jsx
+++ src/components/Store/index.jsx
@@ -3,7 +3,8 @@ import './index.css'
 export default function Store() {
     return (
         <div>
-
+            <div href="https://www.etsy.com/shop/CritterbugArts?ref=shop_sugg_market"> Etsy </div>
+            <div href=""> Fiver </div>
         </div>
     )
 }
