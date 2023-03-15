0bf90a3f8c9038203f3b42b7638944943c111c01
JadenLund <lundjaden@gmail.com>
2023-03-14 19:01:39 -0500

    adding all correct links to contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,23 +1,25 @@
 import './index.css'
-import { faGithub, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons'
+import { faArtstation, faDeviantart, faDiscord, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faNewspaper } from '@fortawesome/free-solid-svg-icons'
 
 export default function Contact() {
     return (
         <div>
             <div className='links'>
-                <a target='_blank' className='resume' rel='noreferrer' href='https://docs.google.com/document/d/e/2PACX-1vQ8IWlnrFXAny90vVemPIf9Oj8-iuwY-oeJIeeIWvaTBXi_Y1iWGk_T_YEI_5u-6STodHn2FHO_yXtq/pub'>
-                    <FontAwesomeIcon icon={faNewspaper} color='#99738e' />
+                <a target='_blank' className='Instagram' rel='noreferrer' href='https://www.instagram.com/critterbug.art/'>
+                    <FontAwesomeIcon icon={faInstagram} color='#99738e' />
                 </a>
-                <a target='_blank' className='linkedin' rel='noreferrer' href='https://www.linkedin.com/in/jaden-lund/'>
-                    <FontAwesomeIcon icon={faLinkedin} color='#99738e' />
+                <a target='_blank' className='DeviantArt' rel='noreferrer' href='https://www.deviantart.com/critterbugart'>
+                    <FontAwesomeIcon icon={faDeviantart} color='#99738e' />
                 </a>
-                <a target='_blank' className='github' rel='noreferrer' href='https://github.com/Specklex'>
-                    <FontAwesomeIcon icon={faGithub} color='#99738e' />
+                <a target='_blank' className='Youtube' rel='noreferrer' href='https://www.youtube.com/channel/UCv0NJ7z_5-Sm7Y1QpKzy2eg'>
+                    <FontAwesomeIcon icon={faYoutube} color='#99738e' />
                 </a>
-                <a target='_blank' className='medium' rel='noreferrer' href='https://medium.com/@lundjaden'>
-                    <FontAwesomeIcon icon={faMedium} color='#99738e' />
+                <a target='_blank' className='Discord' rel='noreferrer' href='https://discord.gg/JNgcm7vjNz'>
+                    <FontAwesomeIcon icon={faDiscord} color='#99738e' />
+                </a>
+                <a target='_blank' className='Artstation' rel='noreferrer' href='https://www.artstation.com/critterbugart'>
+                    <FontAwesomeIcon icon={faArtstation} color='#99738e' />
                 </a>
             </div>
         </div>
