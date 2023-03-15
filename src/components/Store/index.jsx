b617eee8be19c731161be06ba1efcd7e8d6a5f4d
JadenLund <lundjaden@gmail.com>
2023-03-15 14:50:34 -0500

    adding store part

--- src/components/Store/index.jsx
+++ src/components/Store/index.jsx
@@ -3,8 +3,9 @@ import './index.css'
 export default function Store() {
     return (
         <div>
-            <div href="https://www.etsy.com/shop/CritterbugArts?ref=shop_sugg_market"> Etsy </div>
-            <div href=""> Fiver </div>
+            <div href="https://www.redbubble.com/people/ArtBySpecks/shop"> Redbubble </div>
+            <div href="https://www.etsy.com/shop/artbyspecks/?etsrc=sdt"> Etsy </div>
+            <div href="no link yet"> Fiverr </div>
         </div>
     )
 }
