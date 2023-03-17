e4b4c81e60238339a5a3d59a6cbfb5b7401457d6
JadenLund <lundjaden@gmail.com>
2023-03-16 20:31:31 -0500

    changing clickable <a> icon sizes

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,4 +1,4 @@
-import './index.css'
+import './index.scss'
 import { faArtstation, faDeviantart, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react';
@@ -10,20 +10,23 @@ export default function Contact() {
     return (
         <div>
             <div className='links'>
-                <a target='_blank' className='Instagram' rel='noreferrer' href='https://www.instagram.com/ArtBySpeckles/'>
+                <a target='_blank' className='instagram-link' rel='noreferrer' href='https://www.instagram.com/ArtBySpeckles/'>
                     <FontAwesomeIcon icon={faInstagram} color='#99738e' />
                 </a>
 
-                <a target='_blank' className='Twitter' rel='noreferrer' href='https://twitter.com/ArtbySpecks'>
+                <a target='_blank' className='twitter-link' rel='noreferrer' href='https://twitter.com/ArtbySpecks'>
                     <FontAwesomeIcon icon={faTwitter} color='#99738e' />
                 </a>
-                <a target='_blank' className='DeviantArt' rel='noreferrer' href='https://www.deviantart.com/artbyspecks'>
+
+                <a target='_blank' className='deviantart-link' rel='noreferrer' href='https://www.deviantart.com/artbyspecks'>
                     <FontAwesomeIcon icon={faDeviantart} color='#99738e' />
                 </a>
-                <a target='_blank' className='Youtube' rel='noreferrer' href='https://www.youtube.com/channel/UCv0NJ7z_5-Sm7Y1QpKzy2eg'>
+
+                <a target='_blank' className='youtube-link' rel='noreferrer' href='https://www.youtube.com/channel/UCv0NJ7z_5-Sm7Y1QpKzy2eg'>
                     <FontAwesomeIcon icon={faYoutube} color='#99738e' />
                 </a>
-                <a target='_blank' className='Artstation' rel='noreferrer' href='https://www.artstation.com/artbyspecks'>
+
+                <a target='_blank' className='artstation-link' rel='noreferrer' href='https://www.artstation.com/artbyspecks'>
                     <FontAwesomeIcon icon={faArtstation} color='#99738e' />
                 </a>
             </div>
